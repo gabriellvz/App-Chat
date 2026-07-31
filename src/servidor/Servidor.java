7528517275ce6756ca4ab09592c68eb66bb97ee6
Emerson Jesus <emersomjesus999@gmail.com>
2026-07-31 17:56:33 -0300

    Update Servidor.java

--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -1,9 +1,6 @@
 package clienteServidor;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,42 +9,20 @@ public class Servidor {
 	public static void main(String[] args) {
         try {
         	ServerSocket serverSocket = new ServerSocket(4000);//inicia um servidor de rede e abre a porta 4000 para se conectar com um cliente
-    		
-    		System.out.println("servidor aguardando conexão!");
-    		
-    		Socket socket = serverSocket.accept();//pausa a execucao e espera um cliente se conectar
-    		
-    		System.out.println("cliente se conectou!");
-    		
-    		//receber mensagem do cliente
-    		
-    		//obter fluxo de entrada do socket e converter os bytes para string por meio do InputStreamReader
-    		InputStreamReader inputReader = new InputStreamReader(socket.getInputStream());
-    		
-    		//criar um leitor de buffer para facilitar a leitura
-    		BufferedReader reader = new BufferedReader(inputReader);
-    		
-    		//enviar mensagem pro cliente
-    		PrintStream saida = new PrintStream(socket.getOutputStream());
-    		
-    		String mensagemDoCliente;
-    		
-    		while((mensagemDoCliente = reader.readLine() ) !=null ) {
-    			System.out.println("cliente: " + mensagemDoCliente);
-    			
-    			saida.println("servidor: " + mensagemDoCliente);		
-    			
-    		}
-    		socket.close();
-    		serverSocket.close();
-			
+    		while(true) {
+    			System.out.println("servidor aguardando conexão!");
+        		
+        		Socket socket = serverSocket.accept();//pausa a execucao, espera um cliente se conectar e retorna um socket
+        		
+        		ServidorThread servidorThread = new ServidorThread(socket);//cria uma thread responsavel por um cliente
+        		servidorThread.start();
+        		
+        		System.out.println("cliente se conectou!");
+        		//serverSocket.close(); criar um if para fechar a conexao
+    		}		
 		}catch(IOException ex) {
 			ex.printStackTrace();
 			
-		}
-		
-		
-
+		}	
 	}
-
 }
